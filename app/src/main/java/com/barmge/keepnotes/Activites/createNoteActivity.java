5e6f66d7b8acd0e2cb6db6fede86d23e47be115d
barmg3 <barmg3@gmail.com>
2021-10-29 22:10:38 +0200

    AddedColorsCode&ModificationUI

--- app/src/main/java/com/barmge/keepnotes/Activites/createNoteActivity.java
+++ app/src/main/java/com/barmge/keepnotes/Activites/createNoteActivity.java
@@ -1,14 +1,20 @@
 package com.barmge.keepnotes.Activites;
 
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +30,10 @@ public class createNoteActivity extends AppCompatActivity {
 
     private EditText noteTitle , noteText;
     private TextView dateTime;
+    private ImageView changeColor , imageColor1 , imageColor2 , imageColor3 , imageColor4 , imageColor5;
+    private LinearLayout colorPicker;
+
+    private String selectedTextColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,12 +48,95 @@ public class createNoteActivity extends AppCompatActivity {
             }
         });
 
+        colorPicker = findViewById(R.id.miscr_color_layout);
+        changeColor = findViewById(R.id.change_color);
+        changeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(colorPicker.getVisibility() != VISIBLE){
+                colorPicker.setVisibility(VISIBLE);
+                }else {
+                    colorPicker.setVisibility(INVISIBLE);
+                }
+
+            }
+        });
+
+//        selectedTextColor = "#00000";
+//        setNoteTextColor();
+        imageColor1 = colorPicker.findViewById(R.id.imageColor1);
+        imageColor2 = colorPicker.findViewById(R.id.imageColor2);
+        imageColor3 = colorPicker.findViewById(R.id.imageColor3);
+        imageColor4 = colorPicker.findViewById(R.id.imageColor4);
+        imageColor5 = colorPicker.findViewById(R.id.imageColor5);
+
+        colorPicker.findViewById(R.id.viewColor1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedTextColor = "#C0C0C0";
+                imageColor1.setImageResource(R.drawable.ic_check);
+                imageColor2.setImageResource(0);
+                imageColor3.setImageResource(0);
+                imageColor4.setImageResource(0);
+                imageColor5.setImageResource(0);
+                setNoteTextColor();
+            }
+        });
+        colorPicker.findViewById(R.id.viewColor2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedTextColor = "#FF80ED";
+                imageColor1.setImageResource(0);
+                imageColor2.setImageResource(R.drawable.ic_check);
+                imageColor3.setImageResource(0);
+                imageColor4.setImageResource(0);
+                imageColor5.setImageResource(0);
+                setNoteTextColor();
+            }
+        });
+        colorPicker.findViewById(R.id.viewColor3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedTextColor = "#C9A0DC";
+                imageColor1.setImageResource(0);
+                imageColor2.setImageResource(0);
+                imageColor3.setImageResource(R.drawable.ic_check);
+                imageColor4.setImageResource(0);
+                imageColor5.setImageResource(0);
+                setNoteTextColor();
+            }
+        });colorPicker.findViewById(R.id.viewColor4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedTextColor = "#ff7373";
+                imageColor1.setImageResource(0);
+                imageColor2.setImageResource(0);
+                imageColor3.setImageResource(0);
+                imageColor4.setImageResource(R.drawable.ic_check);
+                imageColor5.setImageResource(0);
+                setNoteTextColor();
+            }
+        });
+        colorPicker.findViewById(R.id.viewColor5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedTextColor = "#407294";
+                imageColor1.setImageResource(0);
+                imageColor2.setImageResource(0);
+                imageColor3.setImageResource(0);
+                imageColor4.setImageResource(0);
+                imageColor5.setImageResource(R.drawable.ic_check);
+                setNoteTextColor();
+            }
+        });
+
+
         noteText = findViewById(R.id.noteText);
         noteTitle = findViewById(R.id.noteTitle);
         dateTime = findViewById(R.id.dateAndTime);
 
         dateTime.setText(
-                new SimpleDateFormat("dd mm yy HH:mm a" , Locale.getDefault())
+                new SimpleDateFormat("DD MM yy HH:mm a", Locale.getDefault())
                 .format(new Date())
         );
 
@@ -89,4 +182,10 @@ public class createNoteActivity extends AppCompatActivity {
         }
         new saveNoteTask().execute();
     }
+
+    private void setNoteTextColor(){
+        noteText.getTextColors().getDefaultColor();
+        noteText.setTextColor(Color.parseColor(selectedTextColor));
+    }
+
 }
